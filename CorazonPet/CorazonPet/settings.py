b5f77c20b1e43c82a3e93c461b26421d7c12c417
JULIO FIERRO <juliofierro@Mac-mini-de-JULIO.local>
2017-12-08 16:54:43 -0500

    base de datos

--- CorazonPet/CorazonPet/settings.py
+++ CorazonPet/CorazonPet/settings.py
@@ -94,17 +94,28 @@ WSGI_APPLICATION = 'CorazonPet.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'corazonpet',
-        'USER': 'postgres',
-        'PASSWORD': 'backend17',
-        'HOST': 'localhost',
-        'PORT': 5432
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'corazonpet',
+            'USER': 'postgres',
+            'PASSWORD': 'backend17',
+            'HOST': 'localhost',
+            'PORT': 5432
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
