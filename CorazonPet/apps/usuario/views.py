da70048cbd622fd09a7b74e82eded6b58ec38d6c
JULIO FIERRO <juliofierro@Mac-mini-de-JULIO.local>
2017-12-28 13:44:46 -0500

    Se hacen cambios en la vista restablecer_password para que se valide el token

--- CorazonPet/apps/usuario/views.py
+++ CorazonPet/apps/usuario/views.py
@@ -10,7 +10,12 @@ from apps.usuario.models import RecuperarContrasena
 
 def restablecer_password(request, token):
     if request.method == "GET":
-        return render(request, 'mail_templates/app_recuperar_contrasena.html', {"token": token})
+        try:
+            RecuperarContrasena.objects.get(token=token)
+            return render(request, 'mail_templates/app_recuperar_contrasena.html', {"token": token})
+        except RecuperarContrasena.DoesNotExist:
+            return render(request, 'mail_templates/ContrasenaCambiada.html',
+                          {'mensaje': 'No se ha solicitado restablecimiento de contraseña'})
     elif request.method == "POST":
         token = request.POST.get('token')
         password = request.POST.get('pass')
@@ -38,5 +43,3 @@ def restablecer_password(request, token):
         except RecuperarContrasena.DoesNotExist:
             return render(request, 'mail_templates/ContrasenaCambiada.html',
                           {'mensaje': 'No se ha solicitado un restablecimiento de contraseña'})
-
-
